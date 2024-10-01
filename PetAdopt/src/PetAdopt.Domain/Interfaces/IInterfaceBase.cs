6508b679ac75fa2e51272e1f18e56ada58723e87
Natalia.Mangili <nataliaperico89@gmail.com>
2024-10-01 20:27:13 -0300

    Adicionando PetAdopt.Domain no projeto

---
+++ PetAdopt/src/PetAdopt.Domain/Interfaces/IInterfaceBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetAdopt.Domain.Interfaces
+{
+    internal class IInterfaceBase
+    {
+    }
+}
