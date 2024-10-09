17e3dc789d969525690e0b52ed59f87bc18a9001
Natalia.Mangili <nataliaperico89@gmail.com>
2024-10-08 21:51:19 -0300

    Classe de retorno Result

---
+++ PetAdopt/src/PetAdopt.Application/Result.cs
@@ -0,0 +1,19 @@
+﻿namespace PetAdopt.Application;
+
+public class Result
+{
+    public bool Success { get; }
+    public string Message { get; }
+    public string ErrorCode { get; }
+
+    private Result(bool success, string message = null, string errorCode = null)
+    {
+        Success = success;
+        Message = message;
+        ErrorCode = errorCode;
+    }
+
+    public static Result SuccessResult() => new Result(true);
+
+    public static Result Failure(string message, string errorCode = null) => new Result(false, message, errorCode);
+}
