db2a481ab8e320a482ec3ebe7d7d54fd5ef68be8
Natalia.Mangili <nataliaperico89@gmail.com>
2024-10-20 11:11:54 -0300

    Ajuste de configurações do MongoDb

--- PetAdopt/src/PetAdopt.Infrastructure/MongoLog/MongoDBExtensions.cs
+++ PetAdopt/src/PetAdopt.Infrastructure/MongoLog/MongoDBExtensions.cs
@@ -1,11 +1,8 @@
 ﻿using CrossCutting.Logging;
-using Microsoft.Extensions.DependencyInjection;
+
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
-using PetAdopt.Domain.Interfaces;
 using PetAdopt.Infrastructure.Logging;
-using PetAdopt.Infrastructure.Persistence.Repositories;
-using System.Configuration;
 
 namespace PetAdopt.Infrastructure.MongoLog;
 
@@ -13,24 +10,33 @@ public static class MongoDBExtensions
 {
     public static IServiceCollection ConfigureMongo(this IServiceCollection services, IConfiguration configuration)
     {
-       //services.Configure<MongoDBSettings>(configuration.GetSection("MongoDB"));
-
-        configuration.GetSection("MongoDB");
-
-        services.AddSingleton<IMongoClient, MongoClient>(sp =>
+        var mongoSettings = new MongoDBSettings
         {
-            var settings = sp.GetRequiredService<IOptions<MongoDBSettings>>().Value;
+            ConnectionString = configuration.GetSection("MongoDB").GetSection("ConnectionString").Value
+            ?? throw new ArgumentNullException("there's no ConnectionString for MongoDB"),
+
+            DatabaseName = configuration.GetSection("MongoDB").GetSection("DatabaseName").Value 
+            ?? throw new ArgumentNullException("there's no DatabaseName for MongoDB"),
+        };
+
+        services.AddSingleton(mongoSettings);
+
+        services.AddSingleton<IMongoClient>(sp =>
+        {
+            var settings = sp.GetRequiredService<MongoDBSettings>();
             return new MongoClient(settings.ConnectionString);
         });
 
+        // AddScoped: Uma nova instância é criada para cada solicitação
         services.AddScoped<IMongoDatabase>(sp =>
         {
-            var settings = sp.GetRequiredService<IOptions<MongoDBSettings>>().Value;
+            var settings = sp.GetRequiredService<MongoDBSettings>();
+
+            // IMongoClient que foi registrada aqui
             var client = sp.GetRequiredService<IMongoClient>();
             return client.GetDatabase(settings.DatabaseName);
         });
-
-        //DI
+       
         services.AddScoped<ILogRepository, LogRepository>();
         services.AddScoped<ILogService, LogService>();
 
