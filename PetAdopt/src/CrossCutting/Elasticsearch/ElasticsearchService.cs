25799304e3e3dd9adce280c4407067a36b69a228
Natalia.Mangili <nataliaperico89@gmail.com>
2024-12-08 09:38:40 -0300

    feat: Criando ElasticSearch Service

---
+++ PetAdopt/src/CrossCutting/Elasticsearch/ElasticsearchService.cs
@@ -0,0 +1,36 @@
+﻿using Nest;
+
+namespace CrossCutting.Elasticsearch;
+
+public class ElasticsearchService(IElasticClient elasticClient) : IElasticsearchService
+{
+    private readonly IElasticClient _elasticClient = elasticClient;
+
+    public async Task IndexDocumentAsync<T>(T document, string indexName) where T : class
+    {
+        IndexResponse response = await _elasticClient.IndexAsync(document, idx => idx.Index(indexName));
+        if (!response.IsValid)
+        {
+            throw new Exception($"Error: {response.ServerError?.Error?.Reason}");
+        }
+    }
+
+    public async Task<ISearchResponse<T>> SearchDocumentsAsync<T>(string query, string field) where T : class
+    {
+        ISearchResponse<T> response = await _elasticClient.SearchAsync<T>(s => s
+            .Query(q => q
+                .Match(m => m
+                    .Field(field)
+                    .Query(query)
+                )
+            )
+        );
+
+        if (!response.IsValid)
+        {
+            throw new Exception($"Search Error: {response.ServerError?.Error?.Reason}");
+        }
+
+        return response;
+    }
+}
---
+++ PetAdopt/src/CrossCutting/Elasticsearch/IElasticsearchService.cs
@@ -0,0 +1,9 @@
+﻿using Nest;
+
+namespace CrossCutting.Elasticsearch;
+
+public interface IElasticsearchService
+{
+    Task IndexDocumentAsync<T>(T document, string indexName) where T : class;
+    Task<ISearchResponse<T>> SearchDocumentsAsync<T>(string query, string field) where T : class;
+}
